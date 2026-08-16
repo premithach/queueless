106c9607a6e3cfedc1186d3c25fdad8d79f648d8
Premithach <premithachennupati@gmail.com>
2026-08-16 22:42:28 +0530

    Add CORS support in server.js to allow requests from the QueueLess frontend, including handling preflight requests.

--- queueless-be/src/server.js
+++ queueless-be/src/server.js
@@ -7,6 +7,26 @@ const { handleRequest } = require('./routes');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+  // Allow requests from QueueLess frontend
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+  );
+
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
+
+  // Handle CORS preflight request
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
   handleRequest(req, res);
 });
 
